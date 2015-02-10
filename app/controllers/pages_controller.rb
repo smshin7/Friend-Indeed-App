b6b89b333d7d1612157a39a28b9eab64332a05de
jolamola <mrdanielkim@gmail.com>
2015-02-09 20:41:39 -0800

    fb ref working

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,14 @@
+class PagesController < ApplicationController
+	def homepage
+		if params[:ref_id]
+			cookies[:referral] = params[:ref_id]
+		end
+
+		if current_user
+			redirect_to users_path(current_user)
+		else
+			render :homepage
+		end
+
+	end
+end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,18 @@
 class SessionsController < ApplicationController
-
+	
+  # Method creates session for user and sets the users referrer to the cookie set in
   def create
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user["id"]
-    redirect_to root_path
+    if user.referrer == nil
+      if cookies[:referral]
+        user.update(referrer: cookies[:referral])
+      end
+    redirect_to root_path 
+    end
   end
 
+  # Method to sign out users
   def destroy
     session[:user_id] = nil
     redirect_to root_path
