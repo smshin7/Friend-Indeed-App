1ba2c3c1e9a6eee6a0a7569881feebfb5e9d8368
smshin7 <scottshin1@gmail.com>
2015-02-11 14:30:22 -0800

    added a random token generator to user model

--- app/models/apikey.rb
+++
@@ -1,9 +0,0 @@
-class ApiKey < ActiveRecord::Base
-
-  before_create :generate_token
-
-  def generate_token
-    self.token = SecureRandom.urlsafe_base64
-  end
-  
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150211021524) do
+ActiveRecord::Schema.define(version: 20150211221847) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "apikeys", force: :cascade do |t|
-    t.string   "token"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "deeds", force: :cascade do |t|
     t.string   "body"
@@ -39,6 +33,7 @@ ActiveRecord::Schema.define(version: 20150211021524) do
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
     t.string   "referrer"
+    t.string   "token"
   end
 
 end
