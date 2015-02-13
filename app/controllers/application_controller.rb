3f2cd8f56336a6ac822e0a28c14f226341f54943
jolamola <mrdanielkim@gmail.com>
2015-02-12 21:47:36 -0800

    first front commit, sorry guys

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :set_deed
+
+  def set_deed
+  	@deed = Deed.new
+  end
+
+
   private
   
   def current_user
