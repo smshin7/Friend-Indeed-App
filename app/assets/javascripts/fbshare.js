4cc247dd680b4ee61e56bd4cde3a0b0559034446
smshin7 <scottshin1@gmail.com>
2015-03-25 16:40:30 -0700

    edited fbshare

--- app/assets/javascripts/fbshare.js
+++ app/assets/javascripts/fbshare.js
@@ -1,10 +1,9 @@
 // $(document).ready(function() {
 
 // Javascript Code for Facebook Share button
-FB = null;
 (function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
-  // if (d.getElementById(id)) return;
+  if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
   js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=1410296982600187&version=v2.0";
   fjs.parentNode.insertBefore(js, fjs);
